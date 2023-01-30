40acf39f13b621b01efe96f020e8aa43524f95c1
Papa Raziel <123668233+PapaRaziel@users.noreply.github.com>
2023-01-30 14:02:26 -0500

    Create Policer.ts

---
+++ Scr/Data/Policer.ts
@@ -0,0 +1,25 @@
+export const rageResponses = [
+    'Calm down!',
+    'Too much caps.',
+    'Use your indoor voice.',
+    'Pipe down, we can hear you fine.',
+    'Turn your caps lock off please.',
+    'Caps?',
+    'Please stop with the caps.'
+];
+
+export const tooLongResponses = [
+    'Nice text wall.',
+    'Umm did we really need all that text.',
+    'Are you trying to build a wall?',
+    'Calm down there trump, we don\'t need any walls here.',
+    'You talk too much.'
+];
+
+export const profanityResponses = [
+    'Do you kiss your mother with that mouth?',
+    'Nice potty mouth.',
+    'Stop fnaffing swearing!',
+    'Watch your mouth',
+    'Stop swearing. We don\'t need that here'
+];
