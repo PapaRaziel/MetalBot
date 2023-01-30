32c4adaf52561ee3294d436a9f63ea830212d838
Papa Raziel <123668233+PapaRaziel@users.noreply.github.com>
2023-01-30 14:00:01 -0500

    Create Memes.ts

---
+++ Scr/Data/Memes.ts
@@ -0,0 +1,26 @@
+// Memes can be found and added here.
+export default {
+    'onedoesnot': 61579,
+    'leocheers': 5496396,
+    'brian': 61583,
+    'yuno': 61527,
+    'gollum': 681831,
+    'morpheus': 100947,
+    'doge': 8072285,
+    'fry': 61520,
+    'eviltoddler': 235589,
+    'drevil': 40945639,
+    'batman': 438680,
+    'mostinteresting': 61532,
+    'xxeverywhere': 347390,
+    'wonka': 61582,
+    'wat': 14230520,
+    'bill': 56225174,
+    'justwait': 109765,
+    '10guy': 101440,
+    'grammarcat': 21595223,
+    'onlyone': 259680,
+    'kys': 172314,
+    'christ': 17699,
+    'seal': 13757816
+};
